e9fd5ad69ec282af5856c69d5fce39a737b1553a
Juan Aguirre <juanaguirre237288@correo.itm.edu.co>
2018-12-02 11:30:30 -0500

    First commit: Project's base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Afiliados JAC
+
+Repositorio para el desarrollo del proyecto de Afiliados JAC
---
+++ controller/ctrConecction.php
@@ -0,0 +1,37 @@
+<?
+class CtrConnection
+{
+	var $link;
+	function connect($server, $db, $user, $password)
+	{
+		try {
+			$link = mysql_connect($server, $user, $password);
+		} catch (Exception $e) {
+			echo "ERROR SERVER Connection " . $e->getMessage() . "\n";
+		}
+		try {
+			mysql_select_db($db, $link);
+		} catch (Exception $e) {
+			echo "ERROR FINDING THE DATABASE" . $e->getMessage() . "\n";
+		}
+		return $link;
+	}
+	function close($link)
+	{
+		try {
+			mysql_close($link);
+		} catch (Exception $e) {
+			echo "ERROR CLOSING SERVER Connection" . $e->getMessage() . "\n";
+		}
+	}
+	function executeSQL($db, $select)
+	{
+		try {
+			$recordSet = mysql_db_query($db, $select);
+		} catch (Exception $e) {
+			echo " ERROR EXECUTING SQL TASK: " . $e->getMessage() . "\n";
+		}
+		return $recordSet;
+	}
+}
+?>
